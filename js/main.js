6dbfdcbe0e3a5cce1728c30fcffd069625ab7af1
enasmadina <enasmadina4@gmail.com>
2023-04-09 06:00:18 +0200

    asynchrouns for header of a  to it is section

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function () {
         e.preventDefault();
 
         $("html , body ").animate({
-            scrollTop: $('#'+ $(this).data('scroll')).offset().top
+            scrollTop: $('#'+ $(this).data('scroll')).offset().top + 1
         },1000)
     })
   // to put class active  when  click "a" it  and remove another 
@@ -21,5 +21,19 @@ $(function () {
     // $("li a").removeClass("active");
     // $(this).addClass("active");
      
+  })
+  // to make asynchrouns for header of a  to it is section 
+  $(window).scroll(function () {
+    $(".block").each(function () {
+        if($(window).scrollTop() > $(this).offset().top){
+        //   console.log($(this).attr('id'))
+          var blockID =$(this).attr('id');
+          $("li a").removeClass("active");
+          $('nav li a[data-scroll="'+ blockID +'"] ').addClass("active");
+          
+
+        }  
+    })
+   
   })
 })
