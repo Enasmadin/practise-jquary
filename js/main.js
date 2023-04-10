eed7238e7b24708a4939e35abca24e179180e500
enasmadina <enasmadina4@gmail.com>
2023-04-10 23:43:44 +0200

    click on scroll to Go UP

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
     'use strict';
 
   // to make aheader fixed by height nav 
+
   $("body").css("paddingTop",$('nav').innerHeight());
 
     $("nav li a ").on("click",function (e) {
@@ -12,7 +13,10 @@ $(function () {
             scrollTop: $('#'+ $(this).data('scroll')).offset().top + 1
         },1000)
     })
+
+
   // to put class active  when  click "a" it  and remove another 
+
   $("li a").on("click",function () {
      $(this).addClass("active").parent().siblings().find("a").removeClass("active");
                  // this is another soluation 
@@ -22,8 +26,11 @@ $(function () {
     // $(this).addClass("active");
      
   })
-  // to make asynchrouns for header of a  to it is section 
+
+ 
   $(window).scroll(function () {
+     // to make asynchrouns for header of a  to it is section 
+
     $(".block").each(function () {
         if($(window).scrollTop() > $(this).offset().top){
         //   console.log($(this).attr('id'))
@@ -34,6 +41,28 @@ $(function () {
 
         }  
     })
-   
+
+  // click on scroll to Go UP 
+
+ 
+   var scrollTop = $(".scroll-to-top");
+    
+    if($(window).scrollTop() >= 1000){
+      if($(scrollTop).is(":hidden")){
+        $(scrollTop).fadeIn(5000)
+        $(scrollTop).on("click",function () {
+          $("body,html").animate({
+            scrollTop:0
+          })
+        })
+      }
+    }
+    else{
+      $(scrollTop).fadeOut(5000)
+    }
   })
+
+
+   
+
 })
